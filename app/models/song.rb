5c4310487dc6a3a11c7f0ec71c69e8dde83cba9d
Ali Alrawi <ali.developer90@gmail.com>
2017-01-22 09:14:51 +0300

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,10 @@
+
 class Song < ActiveRecord::Base
   belongs_to :artist
+  has_attached_file :album_cover  , default_url: ':style/default.gif' , styles: { thumb: "100x100>" }
+  validates_attachment_content_type :album_cover, content_type: /\Aimage\/.*\z/
+
+  #validates_attachment_content_type :album_cover, :content_type => /^image\/(jpg|jpeg|pjpeg|png|x-png|gif)$/, :message => 'file type is not allowed (only jpeg/png/gif images)'
 
   def artist_name
     self.try(:artist).try(:name)
